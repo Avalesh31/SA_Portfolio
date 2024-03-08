551ab39940de93efed4225ced1f7f1afb322cefe
Avalesh <sh@fordewindshop.ru>
2024-03-08 22:46:37 +0300

    add new file

---
+++ perimeter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  int perimeter;
+      
+    scanf("%d", &a);
+    scanf("%d", &b);
+   perimeter = 2*(a + b);
+    printf("%d", perimeter);
+    
+  return 0;
+}
